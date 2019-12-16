c1a2acfc13e54993bcf04e5d8e8d40decd19fe41
Kara Alexandra <ardnaxelarak@gmail.com>
2019-12-15 17:29:14 -0800

    Solve puzzle 15a

--- src/RunnableIntcode.java
+++ src/RunnableIntcode.java
@@ -2,6 +2,11 @@ public class RunnableIntcode implements Runnable {
   private final Intcode computer;
   private final IntcodeIO io;
 
+  public RunnableIntcode(long[] memory, IntcodeIO io) {
+    this.computer = new Intcode(memory);
+    this.io = io;
+  }
+
   public RunnableIntcode(int[] memory, IntcodeIO io) {
     this.computer = new Intcode(memory);
     this.io = io;
