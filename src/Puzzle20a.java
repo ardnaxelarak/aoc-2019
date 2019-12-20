b98307890a4aba31e21cf6a2be50ef901343c5f9
Kara Alexandra <ardnaxelarak@gmail.com>
2019-12-19 22:11:14 -0800

    Solve puzzles 20a and 20b

---
+++ src/Puzzle20a.java
@@ -0,0 +1,204 @@
+import com.google.common.collect.ArrayListMultimap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Puzzle20a {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    List<String> lines = new ArrayList<>();
+    while (sc.hasNextLine()) {
+      lines.add(sc.nextLine());
+    }
+
+    char[][] map =
+        lines.stream()
+            .map(String::toCharArray)
+            .toArray(char[][]::new);
+
+    int[][] canTravel =
+        lines.stream()
+            .map(line -> line.chars().map(Puzzle20a::getValue).toArray())
+            .toArray(int[][]::new);
+
+    ArrayListMultimap<String, Point> teleports = ArrayListMultimap.create();
+
+    for (int y = 0; y < map.length; y++) {
+      for (int x = 0; x < map[y].length; x++) {
+        if (isLetter(map[y][x])) {
+          if (isLetter(get(map, y, x + 1))) {
+            String tag = map[y][x] + "" + map[y][x + 1];
+            if (get(map, y, x - 1) == '.') {
+              teleports.put(tag, new Point(x - 1, y));
+            } else if (get(map, y, x + 2) == '.') {
+              teleports.put(tag, new Point(x + 2, y));
+            } else {
+              throw new IllegalStateException();
+            }
+          } else if (isLetter(get(map, y + 1, x))) {
+            String tag = map[y][x] + "" + map[y + 1][x];
+            if (get(map, y - 1, x) == '.') {
+              teleports.put(tag, new Point(x, y - 1));
+            } else if (get(map, y + 2, x) == '.') {
+              teleports.put(tag, new Point(x, y + 2));
+            } else {
+              throw new IllegalStateException();
+            }
+          }
+        }
+      }
+    }
+
+    Map<Point, Point> points = new HashMap<>();
+
+    for (String tag : teleports.keySet()) {
+      List<Point> locs = teleports.get(tag);
+      if (locs.size() == 2) {
+        points.put(locs.get(0), locs.get(1));
+        points.put(locs.get(1), locs.get(0));
+      } else if (locs.size() > 2) {
+        throw new IllegalStateException();
+      }
+    }
+
+    Point start = teleports.get("AA").get(0);
+    Point end = teleports.get("ZZ").get(0);
+    System.out.println(getDistance(canTravel, points, start, end));
+  }
+
+  private static char get(char[][] map, int y, int x) {
+    if (y >= map.length || y < 0) {
+      return ' ';
+    }
+
+    if (x >= map[y].length || x < 0) {
+      return ' ';
+    }
+
+    return map[y][x];
+  }
+
+  private static boolean isLetter(char c) {
+    return c >= 'A' && c <= 'Z';
+  }
+
+  private static int getValue(int c) {
+    if (c == '.') {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  private static class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int getX() {
+      return x;
+    }
+
+    public int getY() {
+      return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof Point)) {
+        return false;
+      }
+      Point p = (Point) o;
+      return p.x == x && p.y == y;
+    }
+
+    @Override
+    public int hashCode() {
+      int hash = 7;
+      hash = hash * 31 + y;
+      hash = hash * 31 + x;
+      return hash;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("<x = %d, y = %d>", x, y);
+    }
+  }
+
+  private static int getDistance(
+      int[][] map, Map<Point, Point> teleports, Point start, Point end) {
+    int curX = start.getX();
+    int curY = start.getY();
+
+    int[][] dist = new int[map.length][map[0].length];
+    for (int y = 0; y < dist.length; y++) {
+      Arrays.fill(dist[y], Integer.MAX_VALUE);
+    }
+
+    List<Point> queue = new ArrayList<>();
+    dist[curY][curX] = 0;
+    queue.add(start);
+
+    Point cur;
+
+    while (!queue.isEmpty()) {
+      cur = queue.remove(0);
+      int cx = cur.getX();
+      int cy = cur.getY();
+
+      if (check(map, dist, cx - 1, cy)) {
+        int curDist = dist[cy][cx] + 1;
+        dist[cy][cx - 1] = curDist;
+        queue.add(new Point(cx - 1, cy));
+      }
+      if (check(map, dist, cx + 1, cy)) {
+        int curDist = dist[cy][cx] + 1;
+        dist[cy][cx + 1] = curDist;
+        queue.add(new Point(cx + 1, cy));
+      }
+      if (check(map, dist, cx, cy - 1)) {
+        int curDist = dist[cy][cx] + 1;
+        dist[cy - 1][cx] = curDist;
+        queue.add(new Point(cx, cy - 1));
+      }
+      if (check(map, dist, cx, cy + 1)) {
+        int curDist = dist[cy][cx] + 1;
+        dist[cy + 1][cx] = curDist;
+        queue.add(new Point(cx, cy + 1));
+      }
+      if (teleports.containsKey(cur)) {
+        Point tp = teleports.get(cur);
+        int tx = tp.getX();
+        int ty = tp.getY();
+        if (dist[ty][tx] == Integer.MAX_VALUE) {
+          dist[ty][tx] = dist[cy][cx] + 1;
+          queue.add(new Point(tx, ty));
+        }
+      }
+    }
+
+    return dist[end.getY()][end.getX()];
+  }
+
+  private static boolean check(int[][] map, int[][] dist, int tx, int ty) {
+    if (ty < 0 || ty >= map.length || tx < 0 || tx >= map[ty].length) {
+      return false;
+    }
+
+    if (dist[ty][tx] < Integer.MAX_VALUE) {
+      return false;
+    }
+
+    return map[ty][tx] > 0;
+  }
+}
---
+++ src/Puzzle20b.java
@@ -0,0 +1,278 @@
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.Table;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Puzzle20b {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    List<String> lines = new ArrayList<>();
+    while (sc.hasNextLine()) {
+      lines.add(sc.nextLine());
+    }
+
+    char[][] map =
+        lines.stream()
+            .map(String::toCharArray)
+            .toArray(char[][]::new);
+
+    int[][] canTravel =
+        lines.stream()
+            .map(line -> line.chars().map(Puzzle20b::getValue).toArray())
+            .toArray(int[][]::new);
+
+    BiMap<String, Point> teleports = HashBiMap.create();
+
+    for (int y = 0; y < map.length; y++) {
+      for (int x = 0; x < map[y].length; x++) {
+        if (isLetter(map[y][x])) {
+          if (isLetter(get(map, y, x + 1))) {
+            String tag = map[y][x] + "" + map[y][x + 1];
+            if (get(map, y, x - 1) == '.') {
+              if (x > map[0].length / 2) {
+                teleports.put("O" + tag, new Point(x - 1, y));
+              } else {
+                teleports.put("I" + tag, new Point(x - 1, y));
+              }
+            } else if (get(map, y, x + 2) == '.') {
+              if (x > map[0].length / 2) {
+                teleports.put("I" + tag, new Point(x + 2, y));
+              } else {
+                teleports.put("O" + tag, new Point(x + 2, y));
+              }
+            } else {
+              throw new IllegalStateException();
+            }
+          } else if (isLetter(get(map, y + 1, x))) {
+            String tag = map[y][x] + "" + map[y + 1][x];
+            if (get(map, y - 1, x) == '.') {
+              if (y > map.length / 2) {
+                teleports.put("O" + tag, new Point(x, y - 1));
+              } else {
+                teleports.put("I" + tag, new Point(x, y - 1));
+              }
+            } else if (get(map, y + 2, x) == '.') {
+              if (y > map.length / 2) {
+                teleports.put("I" + tag, new Point(x, y + 2));
+              } else {
+                teleports.put("O" + tag, new Point(x, y + 2));
+              }
+            } else {
+              throw new IllegalStateException();
+            }
+          }
+        }
+      }
+    }
+
+    Table<String, String, Integer> distances = getDistances(canTravel, teleports);
+
+    String start = "OAA0";
+    Map<String, Integer> visited = new HashMap<>();
+    Map<String, Integer> unvisited = new HashMap<>();
+    unvisited.put(start, 0);
+
+    while (!unvisited.isEmpty()) {
+      String loc =
+          unvisited.entrySet().stream()
+              .min(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
+
+      String baseLoc = loc.substring(0, 3);
+      int level = Integer.parseInt(loc.substring(3));
+
+      int baseDist = unvisited.get(loc);
+      visited.put(loc, baseDist);
+      unvisited.remove(loc);
+
+      if (loc.equals("IZZ-1")) {
+        break;
+      }
+
+      for (String newLoc : distances.columnKeySet()) {
+        if (!distances.contains(baseLoc, newLoc)) {
+          continue;
+        }
+
+        if (newLoc.charAt(0) == 'O') {
+          if (level > 0) {
+            if (newLoc.equals("OAA") || newLoc.equals("OZZ")) {
+              continue;
+            }
+          } else {
+            if (!newLoc.equals("OAA") && !newLoc.equals("OZZ")) {
+              continue;
+            }
+          }
+        }
+
+        String fullLoc = otherSide(newLoc + level);
+
+        if (visited.containsKey(fullLoc)) {
+          continue;
+        }
+
+        int dist = baseDist + 1 + distances.get(baseLoc, newLoc);
+        
+        if (!unvisited.containsKey(fullLoc) || unvisited.get(fullLoc) > dist) {
+          unvisited.put(fullLoc, dist);
+        }
+      }
+    }
+
+    System.out.println(visited.get("IZZ-1") - 1);
+  }
+
+  private static String otherSide(String loc) {
+    char side = loc.charAt(0);
+    String baseLoc = loc.substring(1, 3);
+    int level = Integer.parseInt(loc.substring(3));
+    if (side == 'O') {
+      return "I" + baseLoc + (level - 1);
+    } else {
+      return "O" + baseLoc + (level + 1);
+    }
+  }
+
+  private static char get(char[][] map, int y, int x) {
+    if (y >= map.length || y < 0) {
+      return ' ';
+    }
+
+    if (x >= map[y].length || x < 0) {
+      return ' ';
+    }
+
+    return map[y][x];
+  }
+
+  private static boolean isLetter(char c) {
+    return c >= 'A' && c <= 'Z';
+  }
+
+  private static int getValue(int c) {
+    if (c == '.') {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  private static class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int getX() {
+      return x;
+    }
+
+    public int getY() {
+      return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof Point)) {
+        return false;
+      }
+      Point p = (Point) o;
+      return p.x == x && p.y == y;
+    }
+
+    @Override
+    public int hashCode() {
+      int hash = 7;
+      hash = hash * 31 + y;
+      hash = hash * 31 + x;
+      return hash;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("<x = %d, y = %d>", x, y);
+    }
+  }
+
+  private static Table<String, String, Integer> getDistances(
+      int[][] map, BiMap<String, Point> teleports) {
+    BiMap<Point, String> teleportInverse = teleports.inverse();
+    Table<String, String, Integer> distances = HashBasedTable.create();
+
+    int[][] dist = new int[map.length][map[0].length];
+
+    for (Map.Entry<String, Point> entry : teleports.entrySet()) {
+      Point start = entry.getValue();
+      int curX = start.getX();
+      int curY = start.getY();
+
+      for (int y = 0; y < dist.length; y++) {
+        Arrays.fill(dist[y], Integer.MAX_VALUE);
+      }
+
+      List<Point> queue = new ArrayList<>();
+      dist[curY][curX] = 0;
+      queue.add(start);
+
+      Point cur;
+
+      while (!queue.isEmpty()) {
+        cur = queue.remove(0);
+        int cx = cur.getX();
+        int cy = cur.getY();
+
+        if (!cur.equals(start) && teleportInverse.containsKey(cur)) {
+          distances.put(entry.getKey(), teleportInverse.get(cur), dist[cy][cx]);
+        }
+
+        if (check(map, dist, cx - 1, cy)) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy][cx - 1] = curDist;
+          queue.add(new Point(cx - 1, cy));
+        }
+        if (check(map, dist, cx + 1, cy)) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy][cx + 1] = curDist;
+          queue.add(new Point(cx + 1, cy));
+        }
+        if (check(map, dist, cx, cy - 1)) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy - 1][cx] = curDist;
+          queue.add(new Point(cx, cy - 1));
+        }
+        if (check(map, dist, cx, cy + 1)) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy + 1][cx] = curDist;
+          queue.add(new Point(cx, cy + 1));
+        }
+      }
+    }
+
+    return distances;
+  }
+
+  private static boolean check(int[][] map, int[][] dist, int tx, int ty) {
+    if (ty < 0 || ty >= map.length || tx < 0 || tx >= map[ty].length) {
+      return false;
+    }
+
+    if (dist[ty][tx] < Integer.MAX_VALUE) {
+      return false;
+    }
+
+    return map[ty][tx] > 0;
+  }
+}
