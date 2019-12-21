c80e55fe1bb8ec675a258d91215b364cecd4c018
Kara Alexandra <ardnaxelarak@gmail.com>
2019-12-21 00:31:26 -0800

    Remove BufferedReader/InputStreamReader wrapper around System.in for puzzle 1

--- src/main/java/Puzzle1a.java
+++ src/main/java/Puzzle1a.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Puzzle1a {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    Scanner sc = new Scanner(System.in);
     int total = 0;
     while (sc.hasNextInt()) {
       int mass = sc.nextInt();
