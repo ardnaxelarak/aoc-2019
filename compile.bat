b40f0edb0d23650d6381e7ea1aa68fc276172d0d
Kara Alexandra <ardnaxelarak@gmail.com>
2019-12-21 00:06:07 -0800

    Mavenify everything

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-javac -d target -cp lib\guava-28.1-jre.jar src/*.java
+mvn compile
--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-java -cp target;lib\guava-28.1-jre.jar %*
+@echo off
+
+for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
+
+mvn exec:java -Dexec.mainClass="%1" -Dexec.args="%ALL_BUT_FIRST%"
