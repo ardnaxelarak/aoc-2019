ef3dec662143ba525fd1829e9e4b94807cb9caa0
Kara Alexandra <ardnaxelarak@gmail.com>
2019-12-21 01:45:54 -0800

    Remove the ineffective Puzzle18a to reduce confusion
    Puzzle18a2 has now be renamed to Puzzle18a

--- src/main/java/Puzzle18a.java
+++ src/main/java/Puzzle18a.java
@@ -1,15 +1,22 @@
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Table;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Puzzle18a {
+public class Puzzle18a2 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     List<String> inputs = new ArrayList<>();
@@ -19,33 +26,123 @@ public class Puzzle18a {
 
     int[][] map =
         inputs.stream()
-            .map(line -> line.chars().map(Puzzle18a::getValue).toArray())
+            .map(line -> line.chars().map(Puzzle18a2::getValue).toArray())
             .toArray(int[][]::new);
 
-    Area a = new Area(map);
-    List<Area> queue = new ArrayList<>();
-    Area sol = null;
-    queue.add(a);
+    Table<Integer, Integer, Integer> distances = getDistances(map);
+
+    Set<Integer> neededKeys =
+        distances.rowKeySet().stream()
+            .filter(loc -> loc > 0)
+            .collect(ImmutableSet.toImmutableSet());
+
+    State state = new State(0, 0, neededKeys);
+    List<State> queue = new ArrayList<>();
+    queue.add(state);
+    State sol = null;
+
     while (!queue.isEmpty()) {
-      a = queue.remove(0);
-      if (a.getKeys().isEmpty()) {
-        if (sol == null || a.getDistance() < sol.getDistance()) {
-          sol = a;
+      state = queue.remove(0);
+      // System.err.println(state.getKeysNeeded());
+      if (state.getKeysNeeded().isEmpty()) {
+        if (sol == null || state.getDistance() < sol.getDistance()) {
+          sol = state;
         }
       } else {
-        queue.addAll(getChildren(a));
+        addStates(queue, state.getNextSteps(distances));
       }
     }
 
     System.out.println(sol.getDistance());
   }
 
-  private static ImmutableList<Area> getChildren(final Area a) {
-    return a.getKeys().stream()
-        .map(key -> a.moveTo(key))
-        .filter(Optional::isPresent)
-        .map(Optional::get)
-        .collect(ImmutableList.toImmutableList());
+  private static void addStates(List<State> queue, List<State> toAdd) {
+    for (State s : toAdd) {
+      Optional<State> similar = queue.stream().filter(s::sameBase).findAny();
+      if (similar.isPresent()) {
+        if (similar.get().getDistance() > s.getDistance()) {
+          queue.remove(similar.get());
+          queue.add(s);
+        }
+      } else {
+        queue.add(s);
+      }
+    }
+  }
+
+  private static class State {
+    private int curLoc;
+    private int distance;
+    private ImmutableSet<Integer> keysNeeded;
+
+    public State(int curLoc, int distance, Set<Integer> keysNeeded) {
+      this.curLoc = curLoc;
+      this.distance = distance;
+      this.keysNeeded = ImmutableSet.copyOf(keysNeeded);
+    }
+
+    public List<State> getNextSteps(Table<Integer, Integer, Integer> distances) {
+      Map<Integer, Integer> visited = new HashMap<>();
+      Map<Integer, Integer> unvisited = new HashMap<>();
+      List<State> nextSteps = new ArrayList<>();
+      unvisited.put(curLoc, distance);
+
+      while (!unvisited.isEmpty()) {
+        int loc =
+            unvisited.entrySet().stream()
+                .min(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
+
+        int baseDist = unvisited.get(loc);
+        visited.put(loc, baseDist);
+        unvisited.remove(loc);
+
+        if (keysNeeded.contains(loc)) {
+          continue;
+        }
+
+        for (int newLoc : distances.columnKeySet()) {
+          if (!distances.contains(loc, newLoc)) {
+            continue;
+          }
+
+          if (newLoc < 0 && keysNeeded.contains(-newLoc)) {
+            continue;
+          }
+
+          if (visited.containsKey(newLoc)) {
+            continue;
+          }
+
+          int dist = baseDist + distances.get(loc, newLoc);
+          
+          if (!unvisited.containsKey(newLoc) || unvisited.get(newLoc) > dist) {
+            unvisited.put(newLoc, dist);
+          }
+        }
+      }
+
+      return visited.entrySet().stream()
+          .filter(entry -> keysNeeded.contains(entry.getKey()))
+          .map(
+              entry -> new State(entry.getKey(), entry.getValue(), getKeys(entry.getKey())))
+          .collect(ImmutableList.toImmutableList());
+    }
+
+    private Set<Integer> getKeys(int collected) {
+      return keysNeeded.stream().filter(key -> key != collected).collect(ImmutableSet.toImmutableSet());
+    }
+
+    public int getDistance() {
+      return distance;
+    }
+
+    public Set<Integer> getKeysNeeded() {
+      return keysNeeded;
+    }
+
+    public boolean sameBase(State other) {
+      return curLoc == other.curLoc && keysNeeded.equals(other.keysNeeded);
+    }
   }
 
   private static int getValue(int c) {
@@ -64,47 +161,38 @@ public class Puzzle18a {
     }
   }
 
-  public static class Area {
-    private int[][] map;
-    private int distance;
-    private int curX, curY;
-    private ImmutableList<Integer> keys;
-
-    private Area(int[][] map, int distance, int curX, int curY, List<Integer> keys) {
-      this.map = copyOf(map);
-      this.distance = distance;
-      this.curX = curX;
-      this.curY = curY;
-      this.keys = ImmutableList.copyOf(keys);
+  private static Optional<Integer> check(int[][] map, int[][] dist, int tx, int ty) {
+    if (ty < 0 || ty >= map.length || tx < 0 || tx >= map[ty].length) {
+      return Optional.empty();
     }
 
-    public Area(int[][] map) {
-      this.map = copyOf(map);
-      distance = 0;
-      Point cur = find(map, 0);
-      curX = cur.getX();
-      curY = cur.getY();
-      keys = findKeys(map);
+    if (dist[ty][tx] < Integer.MAX_VALUE) {
+      return Optional.empty();
     }
 
-    public List<Integer> getKeys() {
-      return keys;
+    if (map[ty][tx] == -1) {
+      return Optional.empty();
     }
 
-    public int getDistance() {
-      return distance;
-    }
+    return Optional.of(map[ty][tx]);
+  }
 
-    public Optional<Area> moveTo(int target) {
-      Point dest = find(map, target);
-      int dx = dest.getX();
-      int dy = dest.getY();
+  private static Table<Integer, Integer, Integer> getDistances(int[][] map) {
+    Table<Integer, Integer, Integer> distances = HashBasedTable.create();
+    Map<Integer, Point> landmarks = getLandmarks(map);
+    Optional<Integer> spot;
 
-      Point cur = Point.create(curX, curY);
+    for (Map.Entry<Integer, Point> entry : landmarks.entrySet()) {
+      Point cur = entry.getValue();
+      int curX = cur.getX();
+      int curY = cur.getY();
+      int origin = entry.getKey();
+
       int[][] dist = new int[map.length][map[0].length];
       for (int y = 0; y < dist.length; y++) {
         Arrays.fill(dist[y], Integer.MAX_VALUE);
       }
+
       List<Point> queue = new ArrayList<>();
       dist[curY][curX] = 0;
       queue.add(cur);
@@ -114,105 +202,65 @@ public class Puzzle18a {
         int cx = cur.getX();
         int cy = cur.getY();
 
-        if (check(map, dist, cx - 1, cy, target)) {
-          dist[cy][cx - 1] = dist[cy][cx] + 1;
-          if (cy == dy && cx - 1 == dx) {
-            return Optional.of(craft(map, dist[cy][cx - 1], cx - 1, cy, target));
+        spot = check(map, dist, cx - 1, cy);
+        if (spot.isPresent()) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy][cx - 1] = curDist;
+
+          if (spot.get() == 1) {
+            queue.add(Point.create(cx - 1, cy));
+          } else {
+            distances.put(origin, spot.get(), curDist);
           }
-          queue.add(Point.create(cx - 1, cy));
         }
-        if (check(map, dist, cx + 1, cy, target)) {
-          dist[cy][cx + 1] = dist[cy][cx] + 1;
-          if (cy == dy && cx + 1 == dx) {
-            return Optional.of(craft(map, dist[cy][cx + 1], cx + 1, cy, target));
+        spot = check(map, dist, cx + 1, cy);
+        if (spot.isPresent()) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy][cx + 1] = curDist;
+
+          if (spot.get() == 1) {
+            queue.add(Point.create(cx + 1, cy));
+          } else {
+            distances.put(origin, spot.get(), curDist);
           }
-          queue.add(Point.create(cx + 1, cy));
         }
-        if (check(map, dist, cx, cy - 1, target)) {
-          dist[cy - 1][cx] = dist[cy][cx] + 1;
-          if (cy - 1 == dy && cx == dx) {
-            return Optional.of(craft(map, dist[cy - 1][cx], cx, cy - 1, target));
+        spot = check(map, dist, cx, cy - 1);
+        if (spot.isPresent()) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy - 1][cx] = curDist;
+
+          if (spot.get() == 1) {
+            queue.add(Point.create(cx, cy - 1));
+          } else {
+            distances.put(origin, spot.get(), curDist);
           }
-          queue.add(Point.create(cx, cy - 1));
         }
-        if (check(map, dist, cx, cy + 1, target)) {
-          dist[cy + 1][cx] = dist[cy][cx] + 1;
-          if (cy + 1 == dy && cx == dx) {
-            return Optional.of(craft(map, dist[cy + 1][cx], cx, cy + 1, target));
+        spot = check(map, dist, cx, cy + 1);
+        if (spot.isPresent()) {
+          int curDist = dist[cy][cx] + 1;
+          dist[cy + 1][cx] = curDist;
+
+          if (spot.get() == 1) {
+            queue.add(Point.create(cx, cy + 1));
+          } else {
+            distances.put(origin, spot.get(), curDist);
           }
-          queue.add(Point.create(cx, cy + 1));
         }
       }
-
-      return Optional.empty();
     }
 
-    private Area craft(int[][] map, int dist, int cx, int cy, int target) {
-      int[][] newMap = copyOf(map);
-      newMap[cy][cx] = 0;
-      for (int y = 0; y < newMap.length; y++) {
-        for (int x = 0; x < newMap[y].length; x++) {
-          if (newMap[y][x] == -target) {
-            newMap[y][x] = 1;
-          }
-        }
-      }
-
-      List<Integer> keys = new ArrayList<>(this.keys);
-      keys.remove(Integer.valueOf(target));
-
-      return new Area(newMap, this.distance + dist, cx, cy, keys);
-    }
+    return distances;
   }
 
-  private static boolean check(int[][] map, int[][] dist, int tx, int ty, int target) {
-    if (ty < 0 || ty >= map.length || tx < 0 || tx >= map[ty].length) {
-      return false;
-    }
-
-    if (dist[ty][tx] < Integer.MAX_VALUE) {
-      return false;
-    }
-
-    if (map[ty][tx] == 0 || map[ty][tx] == 1 || map[ty][tx] == target) {
-      return true;
-    }
-
-    return false;
-  }
-
-  private static Point find(int[][] map, int target) {
-    boolean found = false;
+  private static Map<Integer, Point> getLandmarks(int[][] map) {
+    Map<Integer, Point> landmarks = new HashMap<>();
     for (int y = 0; y < map.length; y++) {
       for (int x = 0; x < map[y].length; x++) {
-        if (map[y][x] == target) {
-          return Point.create(x, y);
+        if (map[y][x] != 1 && map[y][x] != -1) {
+          landmarks.put(map[y][x], Point.create(x, y));
         }
       }
     }
-
-    throw new IllegalStateException();
-  }
-
-  private static ImmutableList<Integer> findKeys(int[][] map) {
-    ImmutableList.Builder<Integer> keys = ImmutableList.builder();
-
-    for (int y = 0; y < map.length; y++) {
-      for (int x = 0; x < map[y].length; x++) {
-        if (map[y][x] > 1) {
-          keys.add(map[y][x]);
-        }
-      }
-    }
-
-    return keys.build();
-  }
-
-  private static int[][] copyOf(int[][] array) {
-    int[][] newArr = new int[array.length][];
-    for (int i = 0; i < array.length; i++) {
-      newArr[i] = Arrays.copyOf(array[i], array[i].length);
-    }
-    return newArr;
+    return landmarks;
   }
 }
